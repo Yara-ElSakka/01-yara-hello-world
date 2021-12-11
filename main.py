156b81c52827a2b8baf77efe6f226861526d5ecb
Yara ElSakka <95961383+Yara-ElSakka@users.noreply.github.com>
2021-12-11 15:10:33 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+# my first "hello world" code in Python.
+# by yara elsakka.
+# 22.11.21
+# ------------------------------------------
+
+# hashtag is used to comment the code.
+# hashtag is is not read by the computer/compiler.
+# we start to define my variables :)
+
+welcome = "hello yara .. welcome to the python world !!\nI hope you like it :)\n "
+box_y = 50
+box_a = 80
+box_r = box_y + box_a
+
+# my first function/method is print :)
+
+print(box_r)
+print(f"The answer of 50 + 80 = {box_r}")
+print(welcome)
+
+
+# end of code
+
+# extra code
+
+class Car:
+    def sound(self):
+        print("Vroom!")
+
+
+ferrari = Car()
+ferrari.sound()  # calling method "sound" on object "ferrari"
+
+# end of extra code
+
